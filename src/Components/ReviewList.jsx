5d36b8f624fae04bedb13cffbc94e10b33c22dde
Belinda Turvey <119790281+belindat98@users.noreply.github.com>
2023-05-30 16:27:17 +0100

    add loading screen and styling

--- src/Components/ReviewList.jsx
+++ src/Components/ReviewList.jsx
@@ -4,13 +4,19 @@ import ReviewCard from "./ReviewCard";
 
 const ReviewList = () => {
     const [reviews, setReviews] = useState([])
+    const [isLoading, setIsLoading] = useState(true);
     
     useEffect(() => {
         getReviews().then(reviewsFromApi => {
             setReviews(reviewsFromApi)
+            setIsLoading(false)
         })
     }, []);
 
+    if (isLoading) {
+        return <p>Loading...</p>
+    }
+    
     return (
     <>
         <h2>Reviews</h2>
