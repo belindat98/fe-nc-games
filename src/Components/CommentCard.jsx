78e4e4b1714f684dd0bab8b7ef61b57b35225e3f
belindat98 <119790281+belindat98@users.noreply.github.com>
2023-06-02 16:19:22 +0100

    Revert "11 delete comments"

--- src/Components/CommentCard.jsx
+++ src/Components/CommentCard.jsx
@@ -1,17 +1,8 @@
-import DeleteComment from "./DeleteComment"
-import { useContext } from "react";
-import { CurrentUserContext } from "../Contexts/CurrentUser";
-
-const CommentCard = ({comment, comments, setComments}) => {
+const CommentCard = ({comment}) => {
     const postedDate = new Date(comment.created_at)
-    const { currentUser } = useContext(CurrentUserContext);
-
     return <li className="comment-card" key={comment.comment_id}>
-        <section className = "main-comment-body">
         <p className="comment-info">{comment.author} on {postedDate.toDateString()} at {postedDate.toLocaleTimeString()}</p>
         <p>{comment.body}</p>
-        </section>
-        {currentUser === comment.author ? <DeleteComment comment_id={comment.comment_id} setComments={setComments} /> : ""}
         </li>
 }
 
